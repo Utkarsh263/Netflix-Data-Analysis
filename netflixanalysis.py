7b7c188f427f5e1f6faf4e119a42b8cc7b123cd7
Utkarsh <utkarshkohli821@gmail.com>
2025-12-27 19:38:06 +0530

    Add files via upload

---
+++ netflixanalysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.templates.default = "plotly_white"
+
+netflix_data = pd.read_csv("/content/netflix_content_2023.csv")
+
+netflix_data.head()
